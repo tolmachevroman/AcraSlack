c723f49bada254e6ef87adc0d95adaa35e7693df
tolmachevroman <tolmachevroman@gmail.com>
2015-03-11 09:14:33 -0300

    Urls class added

---
+++ AcraSlackSample/app/src/main/java/com/acra/slack/sample/network/Urls.java
@@ -0,0 +1,11 @@
+package com.acra.slack.sample.network;
+
+/**
+ * Created by romantolmachev on 11/3/15.
+ */
+public class Urls {
+
+    public static final String SLACK_ENDPOINT = "https://hooks.slack.com/services";
+
+    public static final String SLACK_CRASH_CHANNEL = "/T03H6CG1F/B03V91KN6/T8JANY51iQygHlckHX8KQ0Zk";
+}

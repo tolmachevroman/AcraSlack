8ef90cc6c0f524fb7ed994ccd772038e69ffa774
tolmachevroman <tolmachevroman@gmail.com>
2015-03-11 09:37:30 -0300

    Remove unused menu

--- AcraSlackSample/app/src/main/java/com/acra/slack/sample/activities/MainActivity.java
+++ AcraSlackSample/app/src/main/java/com/acra/slack/sample/activities/MainActivity.java
@@ -3,8 +3,6 @@ package com.acra.slack.sample.activities;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 
 import com.acra.slack.sample.R;
@@ -18,29 +16,6 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         (findViewById(R.id.button)).setOnClickListener(this);
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     @Override

9ed3f0d8df491d672f44041c57f1ca4fae68cc3f
tolmachevroman <tolmachevroman@gmail.com>
2015-03-11 09:15:54 -0300

    ClientApiDefinitions class added

---
+++ AcraSlackSample/app/src/main/java/com/acra/slack/sample/network/ClientApiDefinitions.java
@@ -0,0 +1,20 @@
+package com.acra.slack.sample.network;
+
+import retrofit.Callback;
+import retrofit.http.Body;
+import retrofit.http.Headers;
+import retrofit.http.POST;
+
+/**
+ * Created by romantolmachev on 11/3/15.
+ */
+public interface ClientApiDefinitions {
+
+    @Headers("Content-Type: application/json, Content-Length: 8190")
+    @POST(Urls.SLACK_CRASH_CHANNEL)
+    void sendCrash(
+            @Body SlackMessageWrapper wrapper,
+            Callback<String> callback
+    );
+
+}
